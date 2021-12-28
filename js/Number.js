7a7e26d66b668f8632d16f67d8a35f3b1ad95291
liam <64713233+liamhmn@users.noreply.github.com>
2021-12-28 15:41:45 -0600

    Update Number.js

--- js/Number.js
+++ js/Number.js
@@ -26,7 +26,7 @@ ma:new Decimal(1)
     baseResource: "points", // Name of resource prestige is based on
     baseAmount() {return player.points}, // Get the current amount of baseResource
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
-    exponent: 0.5, // Prestige currency exponent
+    exponent: 1000.5, // Prestige currency exponent
     gainMult() { // Calculate the multiplier for main currency from bonuses
         mult = new Decimal(1)
         if (inChallenge('I', 11)||inChallenge('I', 12)||inChallenge('I', 31)||inChallenge('I', 32)) mult = mult.times(1e50)
